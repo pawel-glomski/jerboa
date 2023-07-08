d2abc0245b99ca11e08d5fe5fa0904b1fab4dca3
Paweł Głomski <pglomski.dev@gmail.com>
2023-07-08 12:51:09 +0200

    Change base of `PlayerView` to `QWidget`

--- src/jerboa/main.py
+++ src/jerboa/main.py
@@ -1,59 +1,66 @@
 import sys
-# import PyQt5.QtCore as QtCore
-# import PyQt5.QtGui as QtGui
-# import PyQt5.QtMultimedia as QtMedia
+from PyQt5 import QtCore
+# from PyQt5 import QtGui
+# from PyQt5 import QtMultimedia as QtMedia
 import PyQt5.QtWidgets as QtW
 from PyQt5.QtCore import Qt
 
 
-class PlayerView(QtW.QMainWindow):
+class PlayerView(QtW.QWidget):
 
   def __init__(self):
     super().__init__()
+    self._splitter = PlayerView._create_splitter()
+    layout = QtW.QVBoxLayout()
+    layout.addWidget(self._splitter)
+    layout.setContentsMargins(QtCore.QMargins())
+    self.setLayout(layout)
 
-    self._content = QtW.QSplitter()
-    self._content.setMinimumSize(400, 300)
-    self._content.setSizePolicy(QtW.QSizePolicy.Policy.MinimumExpanding,
-                                QtW.QSizePolicy.Policy.MinimumExpanding)
-    self._content.setOrientation(Qt.Vertical)
-    self._content.setStyleSheet('''
+    PlayerView._add_canvas(self._splitter)
+    PlayerView._add_timeline(self._splitter)
+
+  @staticmethod
+  def _create_splitter() -> QtW.QSplitter:
+    splitter = QtW.QSplitter()
+    splitter.setOrientation(Qt.Vertical)
+    splitter.setStyleSheet('''
       QSplitter::handle {
         border-top: 1px solid #413F42;
         margin: 3px 0px;
       }
       ''')
-    self.setCentralWidget(self._content)
+    return splitter
 
-    self._add_canvas()
-    self._add_timeline()
-
-  def _add_canvas(self):
-    canvas = QtW.QFrame()
-    canvas.setFrameShape(QtW.QFrame.Shape.Box)
-    canvas.setMinimumHeight(150)
+  @staticmethod
+  def _add_canvas(splitter: QtW.QSplitter):
+    canvas = QtW.QLabel('canvas')
+    canvas.setFrameShape(QtW.QFrame.Shape.StyledPanel)
+    canvas.setMinimumHeight(50)
     canvas.setSizePolicy(QtW.QSizePolicy.Policy.Expanding, QtW.QSizePolicy.Policy.Expanding)
 
-    idx = self._content.count()
-    self._content.addWidget(canvas)
-    self._content.setStretchFactor(idx, 4)
-    self._content.setCollapsible(idx, False)
+    idx = splitter.count()
+    splitter.addWidget(canvas)
+    splitter.setStretchFactor(idx, 3)
+    splitter.setCollapsible(idx, False)
 
-  def _add_timeline(self):
-    timeline = QtW.QFrame()
+  @staticmethod
+  def _add_timeline(splitter: QtW.QSplitter):
+    timeline = QtW.QLabel('timeline')
     timeline.setMinimumHeight(50)
-    timeline.setFrameShape(QtW.QFrame.Shape.Box)
+    timeline.setFrameShape(QtW.QFrame.Shape.StyledPanel)
     timeline.setSizePolicy(QtW.QSizePolicy.Policy.Expanding, QtW.QSizePolicy.Policy.Expanding)
 
-    idx = self._content.count()
-    self._content.addWidget(timeline)
-    self._content.setStretchFactor(idx, 1)
-    self._content.setCollapsible(idx, True)
+    idx = splitter.count()
+    splitter.addWidget(timeline)
+    splitter.setStretchFactor(idx, 1)
+    splitter.setCollapsible(idx, True)
 
 
 class MainWindow(QtW.QMainWindow):
 
   def __init__(self):
     super().__init__()
+    self.setMinimumSize(640, 360)
 
     self._create_menu_bar()
     self._create_status_bar()
@@ -87,8 +94,8 @@ class JerboaApp:
   def __init__(self, argv: list[str]) -> None:
     self._app = QtW.QApplication(argv)
     self._main_win = MainWindow()
-    available_geometry = self._main_win.screen().availableGeometry()
-    self._main_win.resize(available_geometry.width() // 2, available_geometry.height() // 2)
+    # available_geometry = self._main_win.screen().availableGeometry()
+    # self._main_win.resize(available_geometry.width() // 2, available_geometry.height() // 2)
 
   def run(self) -> int:
     self._main_win.show()
